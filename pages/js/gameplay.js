f34c0a3e3fe212d829d1b47cf7712c1dd8e95b88
michael tio <77330912+michaeltio@users.noreply.github.com>
2023-04-02 21:40:38 +0700

    monster animasi

--- pages/js/gameplay.js
+++ pages/js/gameplay.js
@@ -1,8 +1,12 @@
 let diamonds = 0;
-let monsterAnimation = $("#myImage").attr("src");
-$()
+let monsterAnimation = $("#monster").attr("src");
 
 
 function Hit() {
-
+    console.log("test");
+    $("#monster img").attr("src", "../../sources/gif/monster-hit.gif");
+    setTimeout(function () {
+        console.log("Delayed function executed!");
+        $("#monster img").attr("src", "../../sources/gif/monster-idle.gif");
+    }, 150);
 }
